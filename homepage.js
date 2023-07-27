326b2fd1c63ded1bdd1a6e0d1cb0639319030f11
Lewis Jones <lewisgwynjones@gmail.com>
2023-07-27 22:05:18 +0000

    add javascript function to create click animation for button on homepage

--- homepage.js
+++ homepage.js
@@ -1,5 +1,6 @@
 const textEl = document.getElementById('text')
 const text = '2DAY 2MORROW 2DO!'
+const buttons = document.querySelectorAll('.ripple')
 let idx = 1
 let speed = 180
 
@@ -18,3 +19,26 @@ function writeText() {
 
     setTimeout(writeText, speed)
 }
+
+/*-action for ripple effect on buttons*/
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
