f846a27b0d53ae520bd2ae3cca1cc79b31c1c3dd
Lewis Jones <lewisgwynjones@gmail.com>
2023-07-30 17:45:36 +0000

    add testimonials data for reviews

---
+++ reviews.js
@@ -0,0 +1,26 @@
+const testimonialsContainer =document.querySelector('.testimonials-container')
+const testimonial = document.querySelector('.testimonial')
+const userImage = document.querySelector('.userImage')
+const username = document.querySelector('username')
+const role = document.querySelector('role')
+
+const testimonials = [
+    {
+        name:'Iestyn Jones',
+        position: 'Athlete',
+        photo:'',
+        text: "Efficient, intuitive, and organized. My go-to for staying productive!",
+    },
+    {
+        name:'Emily Edwards',
+        position: 'HR',
+        photo:'',
+        text: "User-friendly interface, excellent features. Simplifies my tasks effortlessly.",
+    },
+    {
+        name:'Ioan Jones',
+        position: 'Production Manager',
+        text: "Seamless experience, responsive design. A must-have for task management efficiency.",
+    }
+
+]
