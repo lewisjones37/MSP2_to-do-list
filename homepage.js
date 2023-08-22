5d4af502d85a890f83e1807c11372ab709a3fe31
Lewis Jones <lewisgwynjones@gmail.com>
2023-08-22 21:07:32 +0000

    add semicolons as instructed by jshint

--- homepage.js
+++ homepage.js
@@ -1,44 +1,44 @@
-const textEl = document.getElementById('text')
-const text = 'Emilys todos'
-const buttons = document.querySelectorAll('.ripple')
-let idx = 1
-let speed = 250
+const textEl = document.getElementById('text');
+const text = 'Emilys todos';
+const buttons = document.querySelectorAll('.ripple');
+let idx = 1;
+let speed = 250;
 
-writeText()
+writeText();
 
 function writeText() {
-    textEl.innerText = text.slice(0, idx)
+    textEl.innerText = text.slice(0, idx);
 
-    idx++
+    idx++;
 
     if(idx > text.length) {
 
-        idx = 1
+        idx = 1;
 
     }
 
-    setTimeout(writeText, speed)
+    setTimeout(writeText, speed);
 }
 
 /*-action for ripple effect on buttons*/
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
-        const x = e.clientX
-        const y = e.clientY
+        const x = e.clientX;
+        const y = e.clientY;
 
-        const buttonTop = e.target.offsetTop
-        const buttonLeft = e.target.offsetLeft
+        const buttonTop = e.target.offsetTop;
+        const buttonLeft = e.target.offsetLeft;
 
-        const xInside = x - buttonLeft
-        const yInside = y - buttonTop
+        const xInside = x - buttonLeft;
+        const yInside = y - buttonTop;
 
-        const circle = document.createElement('span')
-        circle.classList.add('circle')
-        circle.style.top = yInside + 'px'
-        circle.style.left = xInside + 'px'
+        const circle = document.createElement('span');
+        circle.classList.add('circle');
+        circle.style.top = yInside + 'px';
+        circle.style.left = xInside + 'px';
 
-        this.appendChild(circle)
+        this.appendChild(circle);
 
-        setTimeout(() => circle.remove(), 500)
-    })
-})
+        setTimeout(() => circle.remove(), 500);
+    });
+});
