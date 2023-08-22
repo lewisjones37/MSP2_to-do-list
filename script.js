5e867239765fd6ae8e1aa76f6515091683e04599
Lewis Jones <lewisgwynjones@gmail.com>
2023-08-22 21:27:36 +0000

    add missing semi colon and defined todosEl variable in script.js as pointed out by jshint

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const todosUL = document.getElementById('todos')
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
 
-const todos = JSON.parse(localStorage.getItem('todos'))
+const todos = JSON.parse(localStorage.getItem('todos'));
 
 if(todos) {
-    todos.forEach(todo => addTodo(todo))
+    todos.forEach(todo => addTodo(todo));
 }
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    addTodo()
-})
+    addTodo();
+});
 
 function addTodo(todo) {
-    let todoText = input.value
+    let todoText = input.value;
 
     if(todo) {
-        todoText = todo.text
+        todoText = todo.text;
     }
 
     if(todoText) {
-        const todoEl = document.createElement('li')
+        const todoEl = document.createElement('li');
         if(todo && todo.completed) {
-            todoEl.classList.add('completed')
+            todoEl.classList.add('completed');
         }
 
-        todoEl.innerText = todoText
+        todoEl.innerText = todoText;
 
         todoEl.addEventListener('click', () => {
-            todoEl.classList.toggle('completed')
-            updateLS()
-        })
+            todoEl.classList.toggle('completed');
+            updateLS();
+        });
 
         todoEl.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
+            e.preventDefault();
 
-            todoEl.remove()
-            updateLS()
-        })
+            todoEl.remove();
+            updateLS();
+        });
 
-        todosUL.appendChild(todoEl)
+        todosUL.appendChild(todoEl);
 
-        input.value=''
+        input.value='';
 
-        updateLS()
+        updateLS();
     }
 }
 
@@ -70,20 +70,20 @@ todosUL.addEventListener('touchend', (e) => {
     taskToDelete.parentNode.removeChild(taskToDelete);
   }
 
-  updateLS()
+  updateLS();
 });
 
 function updateLS() {
-    todosEl = document.querySelectorAll('li')
+    const todosEl = document.querySelectorAll('li');
 
-    const todos= []
+    const todos= [];
 
     todosEl.forEach(todoEl => {
         todos.push({
             text: todoEl.innerText,
             completed: todoEl.classList.contains('completed')
-        })
-    })
+        });
+    });
 
-    localStorage.setItem('todos', JSON.stringify(todos))
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
